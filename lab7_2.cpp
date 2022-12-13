32b121286dbf38d66df15e13de1b74aa17c0b8d5
aonnapaht <napaht_d@cmu.ac.th>
2022-12-13 17:13:14 +0700

    lab7_2

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,13 +2,30 @@
 
 using namespace std;
 
+int i = 0, P[5000], k, result;
+
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	cout << "Please input k: ";
+	cin >> k;
+	if(k>0){
 	while(i < 5000){
 		P[i] = 2*i+1;
+		if(P[i]%k==0){
+		    result-=P[i];
+		}
+		else{
+		    result+=P[i];
+		}
 		i++;
 	}
-	
-	return 0;
+	    cout << "Result = " << result;
+}
+else
+{
+    cout << "Invalid input!!!";
+}
+
+    
+    return 0;
 }
